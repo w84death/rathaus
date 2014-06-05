aa6e8dc045f53ab7105c4cb5f8adc1d3d55004e4
Krzysztof Tomiak <ktomiak@ioki.com.pl>
2014-06-05 17:20:39 +0200

    minimap enemies

--- web/app/engine/minimap.js
+++ web/app/engine/minimap.js
@@ -22,7 +22,7 @@ var miniMap = {
 
     update: function(){
         this.ctx.clearRect ( 0 , 0 , maps.active.width * this.scale , maps.active.height * this.scale );
-    	
+
         for (var y=0; y < maps.active.height; y++) {
             for (var x=0; x < maps.active.width; x++) {
                 var wall = maps.levels[maps.active.level].walls[y][x];
@@ -43,14 +43,14 @@ var miniMap = {
         var startHandY = player.y + 0.25;
         var endHandX = player.x + 0.25 + Math.cos(player.rot);
         var endHandY = player.y + 0.25 + Math.sin(player.rot);
-        this.ctx.fillStyle = 'red';
+        this.ctx.fillStyle = 'yellow';
         this.ctx.fillRect(
             player.x * this.scale,
             player.y * this.scale,
             this.scale * 0.5,
             this.scale * 0.5
         );
-        this.ctx.strokeStyle = 'red';
+        this.ctx.strokeStyle = 'yellow';
         this.ctx.beginPath();
         this.ctx.moveTo(
             startHandX * this.scale,
@@ -60,6 +60,16 @@ var miniMap = {
             endHandX * this.scale,
             endHandY * this.scale
         );
+        for (var i = 0; i < maps.levels[maps.active.level].enemies.length; i++) {
+            enemy = maps.levels[maps.active.level].enemies[i];
+            this.ctx.fillStyle = 'red';
+            this.ctx.fillRect(
+            enemy.x * this.scale,
+            enemy.y * this.scale,
+            this.scale * 0.5,
+            this.scale * 0.5
+            );
+        }
         this.ctx.stroke();
 
     },
