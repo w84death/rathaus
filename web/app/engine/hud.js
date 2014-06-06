65def3da73fcea7cdab605030da57f425f475a31
CzlowiekImadlo <quak@tlen.pl>
2014-06-06 06:50:52 +0200

    Styled labels

--- web/app/engine/hud.js
+++ web/app/engine/hud.js
@@ -3,8 +3,11 @@ var hud = {
 
     item : null,
 
+    description : null,
+
     init : function () {
         this.item = $("<div class='currentItem'></div>");
+        this.description = $('#description');
 
         $('.viewport').append(this.item);
 
@@ -21,22 +24,28 @@ var hud = {
         var img = $('<img />');
         img.attr('src', item.getImageUrl());
 
+        var label = $('<span>' + item.name + '</span>');
+
         this.item.append(img);
+        this.item.append(label);
     },
 
     reset : function () {
         this.clearCurrentItem();
         player.item = null;
+        this.hideItemDescription();
     },
 
-    showItemDescription: function(){
-            $('#description').empty();
-            var text=$('<span>If you want to open this door, you should find: '+maps.active.key.item.description+'</span>');
-            $('#description').append(text);
+    showItemDescription: function() {
+        this.description.empty();
+        var text=$('<span>If you want to open this door, you should find: <br /> '+maps.active.key.item.description+'</span>');
+        this.description.append(text);
+        this.description.show();
     },
+
     hideItemDescription: function(){
-            $('#description').empty();
-
+        this.description.empty();
+        this.description.hide();
     }
 
 }
