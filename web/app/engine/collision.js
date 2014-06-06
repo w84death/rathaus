10fd40dcbb4ae2f42ba146a638fa46a3ba751031
Krzysztof Tomiak <ktomiak@ioki.com.pl>
2014-06-06 06:49:46 +0200

    cleaning from not important comments

--- web/app/engine/collision.js
+++ web/app/engine/collision.js
@@ -4,19 +4,11 @@ var collision = {
             x : enemy.x,
             y : enemy.y
         };
-
-        /*lowX = Math.floor(newX);
-        lowY = Math.floor(newY);
-        highX = Math.floor(newX) +1;
-        highY= Math.floor(newY) +1;
-        if ( maps.levels[maps.active.level].walls[lowX][lowY] == 0 || maps.levels[maps.active.level].walls[highX][highY] == 0){
-          */  pos.x=newX;
-            pos.y=newY;
-
-        //}
+        pos.x=newX;
+        pos.y=newY;
      return pos;
-    },
-    isBlocking: function(x,y){
+   },
+   isBlocking: function(x,y){
         if (y < 0 || y >= maps.active.height || x < 0 || x >= maps.active.width) {
             return true;
         }
